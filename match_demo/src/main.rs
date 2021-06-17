36b3bfd6083dbaa05fb54d58745e97155adfc7b7
cuixiaorui <cui_xiaorui@126.com>
2021-06-17 11:52:25 +0800

    feat: update option

--- match_demo/src/main.rs
+++ match_demo/src/main.rs
@@ -58,15 +58,31 @@
 // }
 
 // 结合 option 枚举来使用
-fn main() {
-    let five = Some(5);
-    plus_one(five);
-    plus_one(None);
-}
+// fn main() {
+//     let five = Some(5);
+//     plus_one(five);
+//     plus_one(None);
+// }
 
-fn plus_one(x: Option<i32>) -> Option<i32> {
-    match x {
-        Some(i) => Some(i + 1),
-        None => None,
+// fn plus_one(x: Option<i32>) -> Option<i32> {
+//     // match 必须处理所有情况
+//     // 如果这里的 Some 或者 None 的分支有一个没有处理
+//     // 那么编译器就会报错
+//     match x {
+//         Some(i) => Some(i + 1),
+//         None => None,
+//     }
+// }
+
+// 可以使用通配符来处理不想匹配所有的情况
+fn main() {
+    let v = 0u8;
+    // 这里的 v 的情况太多了
+    // 而且我们只需要处理 几个分支就可以了
+    // 所有我们可以用 _ => ()
+    match v {
+        1 => println!("one"),
+        3 => println!("three"),
+        _ => (),
     }
 }
